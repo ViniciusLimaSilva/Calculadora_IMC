a6fcce971b7cfa4f5e896b2e9dc804ea30529a48
ViniciusLimaSilva <vini-toninho@hotmail.com>
2021-03-07 21:41:19 -0300

    Projeto Concluido

---
+++ Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿
+namespace Calculo_IMC
+{
+    partial class Calculo_De_IMC
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculo_De_IMC));
+            this.lbl_texto = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_Peso = new System.Windows.Forms.TextBox();
+            this.txt_Altura = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btn_Calc = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_git = new System.Windows.Forms.Label();
+            this.LinkGitHub = new System.Windows.Forms.LinkLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_texto
+            // 
+            this.lbl_texto.AutoSize = true;
+            this.lbl_texto.Font = new System.Drawing.Font("Fixedsys Excelsior", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_texto.Location = new System.Drawing.Point(225, 9);
+            this.lbl_texto.Name = "lbl_texto";
+            this.lbl_texto.Size = new System.Drawing.Size(209, 30);
+            this.lbl_texto.TabIndex = 0;
+            this.lbl_texto.Text = "Cálculo De IMC";
+            this.lbl_texto.Click += new System.EventHandler(this.lbl_texto_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(623, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 21);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Classificação";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(639, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Obesidade Grau I";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(639, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "SobrePeso";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(639, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 21);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Magreza";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(542, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 21);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "IMC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(639, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Obesidade Grau II ( Severa)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(639, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(264, 21);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Obesidade Grau III (Morbida)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(639, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 21);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Saudável";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(539, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 22);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "< 18,5";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(545, 236);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(59, 22);
+            this.label16.TabIndex = 24;
+            this.label16.Text = ">= 40";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(500, 195);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(104, 22);
+            this.label17.TabIndex = 25;
+            this.label17.Text = "35,0 - 39,9";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(500, 155);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(104, 22);
+            this.label18.TabIndex = 26;
+            this.label18.Text = "30,0 - 34,9";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(500, 120);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(104, 22);
+            this.label19.TabIndex = 27;
+            this.label19.Text = "25,0 - 29,9";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(500, 82);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(104, 22);
+            this.label20.TabIndex = 28;
+            this.label20.Text = "18,5 - 24,9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 63);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(185, 25);
+            this.label10.TabIndex = 29;
+            this.label10.Text = "Informe seu peso (kg)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "INFO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_Peso
+            // 
+            this.txt_Peso.BackColor = System.Drawing.Color.Gray;
+            this.txt_Peso.Location = new System.Drawing.Point(17, 91);
+            this.txt_Peso.Name = "txt_Peso";
+            this.txt_Peso.Size = new System.Drawing.Size(103, 20);
+            this.txt_Peso.TabIndex = 31;
+            this.txt_Peso.TextChanged += new System.EventHandler(this.txt_Peso_TextChanged);
+            this.txt_Peso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Peso_KeyPress);
+            // 
+            // txt_Altura
+            // 
+            this.txt_Altura.BackColor = System.Drawing.Color.Gray;
+            this.txt_Altura.Location = new System.Drawing.Point(17, 145);
+            this.txt_Altura.Name = "txt_Altura";
+            this.txt_Altura.Size = new System.Drawing.Size(103, 20);
+            this.txt_Altura.TabIndex = 33;
+            this.txt_Altura.TextChanged += new System.EventHandler(this.txt_Altura_TextChanged);
+            this.txt_Altura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Altura_KeyPress);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(260, 25);
+            this.label11.TabIndex = 32;
+            this.label11.Text = "Informe sua altura (metro e cm)";
+            // 
+            // btn_Calc
+            // 
+            this.btn_Calc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calc.Location = new System.Drawing.Point(30, 181);
+            this.btn_Calc.Name = "btn_Calc";
+            this.btn_Calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_Calc.TabIndex = 34;
+            this.btn_Calc.Text = "Calcular";
+            this.btn_Calc.UseVisualStyleBackColor = true;
+            this.btn_Calc.Click += new System.EventHandler(this.btn_Calc_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calculo_IMC.Properties.Resources.balança;
+            this.pictureBox1.Location = new System.Drawing.Point(278, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label_git
+            // 
+            this.label_git.AutoSize = true;
+            this.label_git.Location = new System.Drawing.Point(740, 6);
+            this.label_git.Name = "label_git";
+            this.label_git.Size = new System.Drawing.Size(93, 13);
+            this.label_git.TabIndex = 37;
+            this.label_git.Text = "Desenvolvido por:";
+            // 
+            // LinkGitHub
+            // 
+            this.LinkGitHub.AutoSize = true;
+            this.LinkGitHub.Font = new System.Drawing.Font("Open Sans", 7.25F, System.Drawing.FontStyle.Bold);
+            this.LinkGitHub.Location = new System.Drawing.Point(830, 5);
+            this.LinkGitHub.Name = "LinkGitHub";
+            this.LinkGitHub.Size = new System.Drawing.Size(71, 14);
+            this.LinkGitHub.TabIndex = 36;
+            this.LinkGitHub.TabStop = true;
+            this.LinkGitHub.Text = "Vinicius lima";
+            this.LinkGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkGitHub_LinkClicked);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Calculo_De_IMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(905, 278);
+            this.Controls.Add(this.label_git);
+            this.Controls.Add(this.LinkGitHub);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Calc);
+            this.Controls.Add(this.txt_Altura);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_Peso);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl_texto);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Calculo_De_IMC";
+            this.Text = "Cálculo De IMC";
+            this.Load += new System.EventHandler(this.Calculo_De_IMC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_texto;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_Peso;
+        private System.Windows.Forms.TextBox txt_Altura;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btn_Calc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_git;
+        private System.Windows.Forms.LinkLabel LinkGitHub;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_IMC
+{
+    public partial class Calculo_De_IMC : Form
+    {
+        public Calculo_De_IMC()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_texto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Calculadora de IMC, Saiba como está indo sua saúde de forma rápida e prática apenas informando os dados pedidos abaixo e clicando no botão Calcular!!!", "Informativo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Calculo_De_IMC_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Peso_TextChanged(object sender, EventArgs e)
+        {
+
+            txt_Peso.MaxLength = 5;          
+
+             
+        }
+
+        private void txt_Peso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // permite apenas numeros e numeros com virugula
+
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+            {
+                e.Handled = true;
+                MessageBox.Show("Este campo aceita somente números e vírgulas!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+                MessageBox.Show("Este campo aceita somente uma virgula!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void txt_Altura_TextChanged(object sender, EventArgs e)
+        {
+
+            txt_Altura.MaxLength = 5;
+
+          
+
+        }
+
+        private void txt_Altura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            // permite apenas numeros e numeros com virugula
+           
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+            {
+                e.Handled = true;
+                MessageBox.Show("Este campo aceita somente números e vírgulas!!", "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+                MessageBox.Show("Este campo aceita somente uma virgula!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private void btn_Calc_Click(object sender, EventArgs e)
+        {
+            if(txt_Altura.Text != "" && txt_Peso.Text != "")
+            {
+                double imc = Convert.ToDouble(txt_Peso.Text) / (Convert.ToDouble(txt_Altura.Text) * (Convert.ToDouble(txt_Altura.Text)));
+
+                MessageBox.Show("O seu Cálculo de IMC é : " + imc.ToString("N2") + ", fique atento a tabela ao lado para saber ao estado da sua saúde", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                DialogResult confirmar = MessageBox.Show("Deseja Passar o seu resultado para um TXT?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (confirmar.ToString().ToUpper() == "YES")
+                {
+
+                    StreamWriter Salvar_Dados;
+
+                    string user = System.Windows.Forms.SystemInformation.UserName; // pega o nome do usuario
+
+                    var desktop = @"C:\\Users\\"+ user + "\\Desktop\\IMC.txt"; ;//pega o desktop do usuário
+
+              
+
+                    Salvar_Dados = File.CreateText(desktop);
+
+
+                    Salvar_Dados.WriteLine("Peso: " + txt_Peso.Text);
+                    Salvar_Dados.WriteLine("Altura: " + txt_Altura.Text);
+                    Salvar_Dados.WriteLine("IMC : " + imc.ToString("N2"));
+                    Salvar_Dados.WriteLine("Considere uma mudança e habitos alimentares e uma rotina de exercícios se o seu IMC deu acima de saudável");
+
+                    Salvar_Dados.Close();
+
+                    MessageBox.Show("Arquivo salvo no seu Desktop com sucesso!!!!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                }
+
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Dados em branco ou escritos de forma errada, Tente Novamente!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+            
+
+
+        }
+
+        private void LinkGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ViniciusLimaSilva");
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
